6acf9b5b6811624b03f196b2d3638002710e3588
george <e.tihonov@websoft.ru>
2025-02-11 23:18:17 +0300

    Task 3 v1.0

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,7 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+
 public class Deth8 {
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
 public class Needle7 {
+
+    private final Deth8 death;
+
+    //@Autowired
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
