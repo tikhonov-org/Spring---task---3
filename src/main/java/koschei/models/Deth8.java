32af01dd68bbd998ab27b525fb758a359caa8c75
george <e.tihonov@websoft.ru>
2025-02-12 20:45:05 +0300

    Task 3 v1.1

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -2,6 +2,10 @@ package koschei.models;
 
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return "I am Deth";
+    }
 }
